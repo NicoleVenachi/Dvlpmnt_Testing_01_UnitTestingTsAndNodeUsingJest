45c603eed750c05c8e45aeeac6965865f624cc5d
nicolevenachi <ludwing@unicauca.edu.co>
2025-04-27 16:09:10 -0500

    01_Basics_03->Matchers

--- 01_BasicsJest/test/Utils.test.ts
+++ 01_BasicsJest/test/Utils.test.ts
@@ -1,4 +1,4 @@
-import { toUpperCase } from "../app/Utils";
+import { getStringInfo, toUpperCase } from "../app/Utils";
 
 describe("Utils test suite", () => {
   it("should reuturn uppercase", () => {
@@ -11,5 +11,45 @@ describe("Utils test suite", () => {
 
     // assert: (validate assertion)
     expect(actual).toBe(expected);
+  });
+
+  it.only("should return info for a valid string", () => {
+    // arrange:
+    const sut = getStringInfo;
+
+    // act:
+    const actual = sut("My-String");
+
+    // assert:
+    expect(actual.lowerCase).toBe("my-string");
+    expect(actual.extraInfo).toEqual({});
+
+    expect(actual.characters.length).toBe(9);
+    expect(actual.characters).toHaveLength(9);
+
+    expect(actual.characters).toEqual([
+      "M",
+      "y",
+      "-",
+      "S",
+      "t",
+      "r",
+      "i",
+      "n",
+      "g",
+    ]);
+
+    expect(actual.characters).toContain<string>("M");
+
+    // valitate inner array regardless of the order
+    expect(actual.characters).toEqual(
+      expect.arrayContaining(["S", "t", "r", "i", "n", "g", "M", "y", "-"])
+    );
+
+    //check for undefined (in this case it is an {} can't be undefined)
+    expect(actual.extraInfo).not.toBe(undefined);
+    expect(actual.extraInfo).not.toBeUndefined();
+    expect(actual.extraInfo).toBeDefined();
+    expect(actual.extraInfo).toBeTruthy();
   });
 });
