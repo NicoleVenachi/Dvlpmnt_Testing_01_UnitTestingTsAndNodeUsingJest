e173f7189731764e9a7639998ee1eadee7c62506
nicolevenachi <ludwing@unicauca.edu.co>
2025-04-27 15:34:14 -0500

    01_Basics_02->Unit test structure

--- 01_BasicsJest/test/Utils.test.ts
+++ 01_BasicsJest/test/Utils.test.ts
@@ -1,8 +1,15 @@
 import { toUpperCase } from "../app/Utils";
 
 describe("Utils test suite", () => {
-  test("should reuturn uppercase", () => {
-    const result = toUpperCase("abc");
-    expect(result).toBe("ABC");
+  it("should reuturn uppercase", () => {
+    // arrange: (get upper case function)
+    const sut = toUpperCase;
+    const expected = "ABC";
+
+    // act: (Evaluate method)
+    const actual = sut("abc");
+
+    // assert: (validate assertion)
+    expect(actual).toBe(expected);
   });
 });
