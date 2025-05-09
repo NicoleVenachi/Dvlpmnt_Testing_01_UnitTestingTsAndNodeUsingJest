99cf0d454af26975afe9ff207c0c7c7aec66618e
nicolevenachi <ludwing@unicauca.edu.co>
2025-05-08 20:22:19 -0500

    02_TDD->02_PasswordChecker_ChecksOnLower/UppercaseAndLength

--- 02_PasswordChecker/src/test/pass_checker/PasswordChecker.test.ts
+++ 02_PasswordChecker/src/test/pass_checker/PasswordChecker.test.ts
@@ -4,10 +4,55 @@ describe("PasswordChecker test suite", () => {
   let sut: PasswordChecker;
 
   beforeEach(() => {
+    //arrange
     sut = new PasswordChecker();
   });
 
-  it("Should do nothing for the moment", () => {
-    sut.checkPassword();
+  it("Password with less than 8 characters is invalid", () => {
+    //act
+    const actual = sut.checkPassword("1234567");
+
+    //assert
+    expect(actual).toBe(false);
+  });
+
+  it("Password should have at least 8 characters", () => {
+    //act
+    const actual = sut.checkPassword("12345678Ac");
+
+    //assert
+    expect(actual).toBe(true);
+  });
+
+  it("Password with no upper case is invalid", () => {
+    //act
+    const actual = sut.checkPassword("12345abcd");
+
+    //assert
+    expect(actual).toBe(false);
+  });
+
+  it("Password should contain upper case to be valid", () => {
+    //act
+    const actual = sut.checkPassword("12345abcdA");
+
+    //assert
+    expect(actual).toBe(true);
+  });
+
+  it("Password with no lower case is invalid", () => {
+    //act
+    const actual = sut.checkPassword("12345BCDER");
+
+    //assert
+    expect(actual).toBe(false);
+  });
+
+  it("Password should contain lower case to be valid", () => {
+    //act
+    const actual = sut.checkPassword("12345abcdA");
+
+    //assert
+    expect(actual).toBe(true);
   });
 });
